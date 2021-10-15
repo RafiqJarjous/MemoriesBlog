371e92708254553f22bef5f6a9cb50e875941382
RafiqJarjous <92581607+RafiqJarjous@users.noreply.github.com>
2021-10-15 14:14:09 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://lordrafiq:Master36213425@cluster0.dfowk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'USE YOUR DATEBASE INFO FROM MONGODB';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
